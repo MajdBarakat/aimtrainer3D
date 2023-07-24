9dd011d740240e97e92a0e913af674a15b5b77f6
Majd Barakat <78052909+MajdBarakat@users.noreply.github.com>
2023-07-24 15:22:23 +0100

    Implemented SceneInit + clean up

---
+++ src/lib/SceneInit.js
@@ -0,0 +1,87 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import * as dat from 'dat.gui';
+
+export default class SceneInit {
+	constructor(canvasId, animateCallback) {
+		//core components to initialize Three.js app.
+		this.scene = undefined;
+		this.camera = undefined;
+		this.renderer = undefined;
+
+		//camera params;
+		this.fov = 75;
+		this.nearPlane = 0.1;
+		this.farPlane = 1000;
+		this.canvas = document.getElementById(canvasId);
+
+		//additional components.
+		this.clock = undefined;
+		this.stats = undefined;
+		this.gui = undefined;
+
+		//lighting is basically required.
+		this.ambientLight = undefined;
+
+		//animation callbacks
+		this.animateCallback = animateCallback || undefined;
+	}
+
+	initialize() {
+		this.scene = new THREE.Scene();
+		this.camera = new THREE.PerspectiveCamera(
+			this.fov,
+			window.innerWidth / window.innerHeight,
+			1,
+			1000
+		);
+		this.camera.position.z = 8;
+		this.camera.position.y = -0.5;
+
+		//specify a canvas which is already created in the HTML.
+		this.renderer = new THREE.WebGLRenderer({
+			canvas: this.canvas,
+			antialias: true,
+		});
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+		// document.body.appendChild(this.renderer.domElement);
+
+		this.clock = new THREE.Clock();
+		this.stats = Stats();
+		document.body.appendChild(this.stats.dom);
+		this.gui = new dat.GUI();
+
+		// ambient light
+		this.ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+		this.ambientLight.castShadow = true;
+		this.scene.add(this.ambientLight);
+
+		// if window resizes
+		window.addEventListener('resize', () => this.onWindowResize(), false);
+
+		// NOTE: Load space background.
+		// this.loader = new THREE.TextureLoader();
+		// this.scene.background = this.loader.load('./pics/space.jpeg');
+	}
+
+	animate() {
+		this.render();
+		this.stats.update();
+		this.animateCallback && this.animateCallback();
+		window.requestAnimationFrame(this.animate.bind(this));
+	}
+
+	render() {
+		// NOTE: Update uniform data on each render.
+		this.renderer.render(this.scene, this.camera);
+	}
+
+	onWindowResize() {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+	}
+}
